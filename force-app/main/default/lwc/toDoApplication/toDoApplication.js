b4777a39b84acaf9bfc24a0cd419ecbff65123af
gauravkh31 <khandelwalgaurav31@gmail.com>
2025-10-27 15:27:55 +0530

    practice comp

--- force-app/main/default/lwc/toDoApplication/toDoApplication.js
+++ force-app/main/default/lwc/toDoApplication/toDoApplication.js
@@ -3,8 +3,8 @@ import { LightningElement } from 'lwc';
 export default class ToDoApplication extends LightningElement {
     taskname = "";
     taskdate = null;
-    incompletetask = [];
-    completetask = [];
+    incompletetask = []
+    
 
 
     changeHandler(event) {
@@ -27,42 +27,71 @@ resetHandler(event){
     this.taskdate = null;
 }
 
-
-
 addTaskHandler(event){
-// if enddate is missing then today date is end date
-    if(!this.taskdate){
-this.taskdate = new Date().toISOString().slice(0,10);
-}
-
-if(this.validateTask()){
-this.incompletetask = [...this.incompletetask,{
-    taskname : this.taskname,
-    taskdate : this.taskdate
-}]
-
-}
-}
-
-validateTask(){
-
-    
-    let isValid = true;
-    if(!this.taskname){
-        // check if task is empty
-        isValid = false;
-    }else{
-
-        // check if array is containing the same task already, if it do have , it will return task item if not found, it will return undeffined 
-        let taskitem = this.iscompletetask.find(currItem => currItem.taskname === this.taskname && currItem.taskdate === this.taskdate)
-
-        if(taskitem){
-            isValid = false;
-        } 
+    if(this.taskdate == null){
+        this.taskdate = new Date().toISOString().slice(0,10);
+    }
+    if(this.isValid()){
+        this.incompletetask = [...this.incompletetask,{
+            taskname : this.taskname,
+            taskdate : this.taskdate
+        }]
     }
 }
 
-}f
+    isValid(){
+        if(!this.taskname){
+            return false;
+        }
+        else{
+            if(this.incompletetask.find(currItem => 
+                currItem.taskname === this.taskname && currItem.taskdate === this.taskdate
+            )){
+                return false;
+            }
+        }
+        return true;
+    }
 
 
-// 29 minutes
+
+// addTaskHandler(event){
+// // if enddate is missing then today date is end date
+//     if(!this.taskdate){
+// this.taskdate = new Date().toISOString().slice(0,10);
+// }
+
+// if(this.validateTask()){
+// this.incompletetask = [...this.incompletetask,{
+//     taskname : this.taskname,
+//     taskdate : this.taskdate
+// }]
+
+// }
+// }
+
+// validateTask(){
+
+    
+//     let isValid = true;
+//     if(!this.taskname){
+//         // check if task is empty
+//         isValid = false;
+//     }else{
+
+//         // check if array is containing the same task already, if it do have , it will return task item if not found, it will return undeffined 
+//         let taskitem = this.iscompletetask.find(currItem => currItem.taskname === this.taskname && currItem.taskdate === this.taskdate)
+
+//         if(taskitem){
+//             isValid = false;
+//         } 
+//     }
+// }
+
+buttonClick(event){
+    console.log(this.incompletetask);
+    
+}
+
+}
+
