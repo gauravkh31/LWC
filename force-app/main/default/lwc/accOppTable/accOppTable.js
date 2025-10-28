3067c7f200c321d1fa2a4621a28ebed7cb5b1b30
gauravkh31 <khandelwalgaurav31@gmail.com>
2025-10-28 09:27:58 +0530

    pp

--- force-app/main/default/lwc/accOppTable/accOppTable.js
+++ force-app/main/default/lwc/accOppTable/accOppTable.js
@@ -1,3 +1,68 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import getAccWithOpps from '@salesforce/apex/accController.getAccWithOpps';
 
-export default class AccOppTable extends LightningElement {}
+
+const columns = [
+    { label: 'Account Name', fieldName: 'Name' },
+    
+    { label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'currency' },
+    
+];
+
+
+
+
+
+const columnsOpp = [
+    { label: 'Opportunity Name', fieldName: 'Name' },
+    
+    { label: 'Amount', fieldName: 'Amount', type: 'currency' },
+    
+];
+export default class AccOppTable extends LightningElement {
+    columns = columns ;
+    isRowSelected = false; 
+    columnsOpp = columnsOpp
+    @track oppData;
+
+    @track accountData; 
+    connectedCallback(){
+        this.loadData();
+    }
+    
+
+    loadData(){
+        getAccWithOpps().then(result=>{
+            console.log("data is", result);
+            this.accountData = result
+        }).catch(error=>{
+            console.log("error is", error);
+            
+        })
+    }
+
+    handleSelectedRow(event){
+
+        if(event.detail.config.action=='rowDeselect'){
+            this.isRowSelected = false;
+            this.oppData = null
+        }else{
+            this.isRowSelected = true;
+            this.loadOppData(event.detail.selectedRows[0].Id);
+        }
+
+        
+        console.log("is row selected", this.isRowSelected);
+        
+        console.log("selected row data is", JSON.stringify(event.detail));
+        
+    }
+
+    loadOppData(accountId){
+        this.accountData.find(currItem => {
+            if(currItem.Id == accountId){
+                this.oppData = currItem.Opportunities
+            }
+        })
+    }
+}
--- force-app/main/default/lwc/recordEditForm/recordEditForm.js
+++ force-app/main/default/lwc/recordEditForm/recordEditForm.js
@@ -2,9 +2,17 @@ import { LightningElement , api } from 'lwc';
 
 
 export default class RecordEditForm extends LightningElement {
-
+isClicked = false;
 
     @api recordId
     objectApiName = 'Account'
     fields = ['Name', 'Phone', 'Website']
+
+    buttonClickHandler(event){
+        this.isClicked = !this.isClicked;
+    }
+
+    closeClickHandler(){
+        this.isClicked = false;
+    }
 }
